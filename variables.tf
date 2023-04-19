79db9b29e50e55aaf9728c3077144449e3067f70
Sreelekha Mittinti <62982503+Shreelekhashree77@users.noreply.github.com>
2023-04-19 16:26:50 -0400

    code deploy

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "SMITTINT"
+  default     = "smittint"
 }
 
 variable "class_name" {
