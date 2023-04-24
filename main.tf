8a45d4de7752efd643c2ce5268602d57965f5346
Sreelekha Mittinti <62982503+Shreelekhashree77@users.noreply.github.com>
2023-04-24 18:40:32 -0400

    code_changes

--- main.tf
+++ main.tf
@@ -42,3 +42,95 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+resource "azurerm_application_insights" "example" {
+  name                = "workspace-example-ai-smit"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "example" {
+  name                = "mit-workspacevault-new3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_subscription.current.tenant_id
+  sku_name            = "premium"
+}
+
+
+resource "azurerm_machine_learning_workspace" "example" {
+  name                    = "machine-learning-ws-smit"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  application_insights_id = azurerm_application_insights.example.id
+  key_vault_id            = azurerm_key_vault.example.id
+  storage_account_id      = azurerm_storage_account.storage.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "tfex-cosmos-db-${random_integer.deployment_id_suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = "westus"
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_app_service_plan" "webapp_plan" {
+  name                = "webapp-plan-smit"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = "mit-webapp-new"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.webapp_plan.id
+}
+
+output "webapp_url" {
+  value = azurerm_app_service.webapp.default_site_hostname
+}
